945c23fc4c2b8d50c50a361646b2630f932388f9
James Martinez <txjammer1@users.noreply.github.com>
2014-10-18 20:18:08 -0500

    added hex and actually made changes to main.c

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
-#include "avr/io.h"
-#include "avr/interrupt.h"
+#include <inttypes.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
 
 int state=0;
 
 ISR(TIM0_COMPA_vect)
 {
-if(PINB&0x01 != 0x01){
+if((PINB & (1<<PB0)) == 0){
 	//forward pressed
 			switch (state){
 				case 0:
@@ -42,9 +44,7 @@ if(PINB&0x01 != 0x01){
 				break;
 			}
 }
-		
-		else{}
-		//reti();
+		else{}	
 }
 
 
@@ -52,20 +52,16 @@ if(PINB&0x01 != 0x01){
 int main (void){
 
 //setup io pins
-PORTB= (0<<PB4) | (0<<PB3) | (0<<PB2) |(0<<PB1) | (0<<PB0);
+PORTB= (0<<PB4) | (0<<PB3) | (0<<PB2) |(0<<PB1) | (1<<PB0);
 DDRB=  (1<<DDB4) | (1<<DDB3) | (1<<DDB2) | (1<<DDB1) | (0<<DDB0);
 
 //setup timer and interrupt
-OCR0A=0xA; 
+OCR0A|=0xA; 
 TCCR0A|=(1<<COM0A1) | (0<<COM0A0);
 TCCR0B|=(0<<CS02) | (0<<CS01) | (1<<CS00);
 TIMSK0|=(1<<OCIE0A);
 
 sei(); // Enable Global Interrupts
 
-while(1){
-
-
-}
-
+while (1){}
 }
